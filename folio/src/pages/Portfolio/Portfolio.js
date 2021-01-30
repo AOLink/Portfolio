e129630219dda1e8f4a427ca9be3ae1d5816f351
AOLink <aolink101@gmail.com>
2021-01-30 13:10:12 -0500

    Completed General App framework

--- folio/src/pages/Portfolio/Portfolio.js
+++ folio/src/pages/Portfolio/Portfolio.js
@@ -10,38 +10,39 @@ const Portfolio = () => {
 
 
     return (
-        <Grid container className="section pb_45 pt_45">
+        <Grid container spacing={1} className="section pb_45 pt_45">
             
             {/* Title */}
-            <Grid item className="section_title mb_30">
+            <Grid item className="section_title mb_20">
                 <span></span>
                 <h6 className="section_title_text">Portfolio</h6>
             </Grid>
 
             {/* Tabs */}
             <Grid item xs={12}>
-                <Tabs value={tabValue} indicatorColor="white" className="custom_tabs" onChange={(event, newValue) => setTabValue(newValue)}>
+                <Tabs value={tabValue} indicatorColor="white" className="customTabs" onChange={(event, newValue) => setTabValue(newValue)}>
                     <Tab label="All" value="All" className={tabValue == "All" ? 'customTabs_item active' : 'customTabs_item'} />
                     {/* {resumeData.projects.map(item)} */}
                     {[...new Set(resumeData.projects.map(item => item.tag))].map(tag => (
-                        <Tab label={tag} value={tag} className={tabValue == "All" ? 'customTabs_item active' : 'customTabs_item'} />
+                        <Tab label={tag} value={tag} className={tabValue == tag ? 'customTabs_item active' : 'customTabs_item'} />
                     ))}
                 </Tabs>        
             </Grid>
                                                                                                                                                                    
               {/* Projects */}
             <Grid item xs={12}>
-                <Grid container spacing={2}>
+                <Grid container spacing={3}>
                     {resumeData.projects.map((project) => (
                         <>
-                           {tabValue == project.tag || tabValue == "All" ? ( <Grid item>
+                           {tabValue == project.tag || tabValue == "All" ? (
+                            <Grid item xs={12} sm={6} md={4}>
                                 <Grow in timeout={1000}>
                                     <Card className="customCard" onClick={ () => setProjectDialog(project)}>
                                         <CardActionArea>
                                             <CardMedia className="customCard_image" image={project.image} title={project.title}/>
                                             <CardContent>
-                                                <Typography className="customeCard_title">{project.title}</Typography>
-                                                <Typography variant="body2" className="customCard_description">{project.caption}</Typography>
+                                                <Typography variant={'body2'} className="customCard_title">{project.title}</Typography>
+                                                <Typography variant="body2" className="customCard_caption">{project.caption}</Typography>
 
 
                                             </CardContent>
@@ -61,19 +62,19 @@ const Portfolio = () => {
 
             </Grid> 
 
-            <Dialog open={projectDialog} onClose={() => setProjectDialog(false)}>
+            <Dialog open={projectDialog} onClose={() => setProjectDialog(false)} className="projectDialog" fullWidth>
                 <DialogTitle onClose={() => setProjectDialog(false)}>{projectDialog.title}</DialogTitle>
-                <img src="" alt="" />
+                <img src={projectDialog.image} alt="" className="projectDialog_image" />
                 <DialogContent>
-                    {projectDialog.description}
+                    <Typography className="projectDialog_description">{projectDialog.description}</Typography>
                 </DialogContent>
-                <DialogActions>
+                <DialogActions className="projectDialog_actions">
                     {projectDialog?.links?.map(link => (
-                        <a href={link.link} targer="_blank" className=""> {link.icon}</a>
+                        <a href={link.link} target="_blank" className="projectDialog_icon"> {link.icon}</a>
                     ))}
                 </DialogActions>
             </Dialog>
-            
+               
         </Grid>
     )
 }
